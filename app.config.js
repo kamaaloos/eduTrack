81d31031a73fc38caf206cefd18717587457904f
Hasan Aden <kamaaloos@gmail.com>
2026-06-07 00:44:08 +0300

    Fix app.config.js to merge app.json via Expo config callback, Use the ({ config }) pattern so expo-doctor recognizes app.json values and all 18 checks pass

--- app.config.js
+++ app.config.js
@@ -1,18 +1,14 @@
-const appJson = require("./app.json");
-
 /** @type {import('expo/config').ExpoConfig} */
-module.exports = () => ({
-  expo: {
-    ...appJson.expo,
-    plugins: appJson.expo.plugins.map((plugin) => {
-      const name = Array.isArray(plugin) ? plugin[0] : plugin;
-      if (name === "./plugins/withAdiRegistration") {
-        return [
-          "./plugins/withAdiRegistration",
-          { token: process.env.ADI_REGISTRATION_TOKEN },
-        ];
-      }
-      return plugin;
-    }),
-  },
+module.exports = ({ config }) => ({
+  ...config,
+  plugins: config.plugins.map((plugin) => {
+    const name = Array.isArray(plugin) ? plugin[0] : plugin;
+    if (name === "./plugins/withAdiRegistration") {
+      return [
+        "./plugins/withAdiRegistration",
+        { token: process.env.ADI_REGISTRATION_TOKEN },
+      ];
+    }
+    return plugin;
+  }),
 });
